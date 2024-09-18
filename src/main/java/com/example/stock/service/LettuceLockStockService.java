cc0d2d89ec3cff93868d8c56a9d6f7b670de03a7
japygo <hyunnjinn@gmail.com>
2024-09-18 15:02:38 +0900

    Lettuce를 작성하여 재고감소 로직 작성하기

---
+++ src/main/java/com/example/stock/service/LettuceLockStockService.java
@@ -0,0 +1,27 @@
+package com.example.stock.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+
+@Service
+public class LettuceLockStockService {
+
+    private final StockRepository stockRepository;
+
+    public LettuceLockStockService(StockRepository stockRepository) {
+        this.stockRepository = stockRepository;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void decrease(Long id, Long quantity) {
+        Stock stock = stockRepository.findById(id).orElseThrow();
+        stock.decrease(quantity);
+
+        stockRepository.saveAndFlush(stock);
+    }
+
+}
