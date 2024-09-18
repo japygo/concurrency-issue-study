a6205093f112ab7eb4f75646130cb0f6c41dc16d
japygo <hyunnjinn@gmail.com>
2024-09-18 14:02:57 +0900

    재고감소 로직작성

---
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -0,0 +1,42 @@
+package com.example.stock.service;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+
+@SpringBootTest
+class StockServiceTest {
+
+    @Autowired
+    private StockService stockService;
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    @BeforeEach
+    void setUp() {
+        stockRepository.saveAndFlush(new Stock(1L, 100L));
+    }
+
+    @AfterEach
+    void tearDown() {
+        stockRepository.deleteAll();
+    }
+
+    @Test
+    void 재고감소() {
+        stockService.decrease(1L, 1L);
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        assertThat(stock.getQuantity()).isEqualTo(99L);
+    }
+
+}
