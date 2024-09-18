feee94d87455bd68f416c54147643693052c923d
japygo <hyunnjinn@gmail.com>
2024-09-18 14:14:23 +0900

    Synchronized 이용해보기

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -1,7 +1,6 @@
 package com.example.stock.service;
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
@@ -15,8 +14,8 @@ public class StockService {
         this.stockRepository = stockRepository;
     }
 
-    @Transactional
-    public void decrease(Long id, Long quantity) {
+    // @Transactional
+    public synchronized void decrease(Long id, Long quantity) {
         Stock stock = stockRepository.findById(id).orElseThrow();
         stock.decrease(quantity);
 
